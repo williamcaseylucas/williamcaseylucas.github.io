a06cab0e1f5baa6611a862b4e2865e0fb4cff7de
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-06-30 23:34:43 -0400

    updated settings with jsx update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  jsx: {
+    // Accept both .js and .jsx files
+    factory: 'React.createElement',
+    fragment: 'React.Fragment',
+    // Other JSX options...
+  },
   base: "/williamcaseylucas.github.io/",
   plugins: [react()],
   root: '.',
