8ca00f90ee5b9ad2e4dc7a542b2584d441de7e11
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-07-01 16:53:59 -0400

    updated portfolio portion of website

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,9 +5,13 @@ import Title from "./Title";
 
 function Portfolio() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Title>Portfolio</Title>
+    <div className="flex flex-col items-center justify-center w-full">
+      {/* <div className="w-full md:w-3/8 md:pl-20">
+      </div> */}
+      <div className="md:flex-row grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="col-span-full">
+          <Title>Portfolio</Title>
+        </div>
         {portfolio.map((project) => (
           <PortfolioItem
             imgUrl={project.imgUrl}
