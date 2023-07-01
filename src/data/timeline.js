f970ba8d1c5a1c4f46a5880106b2723074814738
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-07-01 14:27:32 -0400

    updated info

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,20 +1,55 @@
 export default [
     {
-        year: 'Today',
-        title: 'Youtube Content Creator',
-        duration: '6 years',
-        details: 'shfjdfjahdfsdjfhjksdhfaljdfh'
+        year: 'May 2023 - August 2023',
+        title: 'Kyndryl - Software Engineer',
+        duration: '3 months',
+        details: ''
     },
     {
-        year: 'Today',
-        title: 'Youtube Content Creator',
+        year: 'Jan 2023 - Present',
+        title: 'Teaching Assistant for CS 4641/7641 Machine Learning',
         duration: '6 years',
-        details: 'shfjdfjahdfsdjfhjksdhfaljdfh'
+        details: 'Explain and demonstrate methods for implementing and applying Machine Learning (ML) concepts and models to data. \
+        Review students ML projects and provide constructive feedback. \
+        Host online office hours weekly. \
+        Assist students through mega threads using edstem.'
     },
     {
-        year: '2013',
-        title: 'Youtube Content Creator',
-        duration: '6 years',
-        details: 'shfjdfjahdfsdjfhjksdhfaljdfh'
+        year: 'Janurary 2022',
+        title: 'Vertically Integrated Project – Experimental Flights, the Georgia Institute of Technology',
+        duration: '4 semesters',
+        details: 'Contributed to the development of a drone delivery system for the delivery of small packages on the Georgia \
+        Tech campus as part of a Vertically Integrated Project (VIP) on Experimental Flights. \
+        '
+    },
+    {
+        year: 'Jan 2022',
+        title: 'Teaching Assistant for CS 1331 Intro to Object Oriented Programming',
+        duration: '1 semester',
+        details: '• Thoroughly explained and clarified how to conceptualize object-oriented concepts/principles for CS 1331 students – Intro to Object Oriented Programming. \
+        Wrote custom tests using Junit and Reflection API to autograde HW assignments. \
+        Graded HW assignments and tests weekly and monthly, respectively. \
+        Held in-person office hours and online office hours weekly. \
+        Answered questions raised on Piazza.'
+    },
+    {
+        year: 'August 2021',
+        title: 'Augmented/Virtual Reality Research at the Georgia Institute of Technology',
+        duration: '1 semester',
+        details: 'Incorporated AR and VR elements into interactive web-based project. \
+        Implement 2D/3D elements into a digital book using VUE.js and TypeScript.js. \
+        Collaborated with faculty and PhD students weekly to accomplish project goals.'
+    },
+    {
+        year: 'June 2020',
+        title: 'Web Content Analyst',
+        duration: '3 years',
+        details: 'Analyzing various web pages and determining which satisfy the users intent best.'
+    },
+    {
+        year: 'Jan 2020',
+        title: 'Education United - Private Math Tutor',
+        duration: '9 months',
+        details: 'Worked as a tutor that taught pre-algebra, algebra I, algebra II, pre-calculus, trigonometry, and calculus. Taught per-calculus class during Summer 2020 with mandatory weekly attendance and grades.'
     },
 ]
