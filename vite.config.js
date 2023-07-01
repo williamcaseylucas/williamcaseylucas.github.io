3fd99f23a627459f476537e64c1d45398bd0fc94
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-07-01 00:02:42 -0400

    try 100000000000

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   jsx: {
-    // Accept both .js and .jsx files
+    // Accept both .js and .jsx files 
     factory: 'React.createElement',
     fragment: 'React.Fragment',
   },
