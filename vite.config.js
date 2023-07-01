11524fabd97729b89c8cc8e47f58d24119bc31b6
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-06-30 20:44:09 -0400

    see if this helps

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./williamcaseylucas.github.io/",
+  base: "/williamcaseylucas.github.io/",
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: './build'
+  }
 })
