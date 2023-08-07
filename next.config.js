d1e695783eea0f0cda0b6535008e53d3f736a5af
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-08-07 10:28:17 -0400

    removed workflow

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: "imgix",
-    path: "",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
