27b96acab3e0365b288d4e7603444afb7e16dda6
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-08-07 09:34:42 -0400

    added basic github pages files for updated portfolio

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "imgix",
+    path: "",
+  },
+};
 
 module.exports = nextConfig;
