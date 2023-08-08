9541a714afbf3d04773d2f1c9ee1770bd9542df7
williamcaseylucas <66389544+williamcaseylucas@users.noreply.github.com>
2023-08-07 20:39:47 -0400

    removed default width

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -442,8 +442,8 @@ export const CircularText = ({ className, ...rest }) => (
 export const LinkArrow = ({ className, ...rest }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
+    // width={24}
+    // height={24}
     viewBox="0 0 24 24"
     className={`w-full h-auto ${className}`}
     {...rest}
